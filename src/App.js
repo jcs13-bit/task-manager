f1058d017102ec1a87ea9b331a1778b191fe70ec
Jose <you@example.com>
2023-01-17 08:23:43 -0500

    proyecto inicial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <h2>How to Add Custom Validatin with Forms in React</h2>
+    </div>
+  );
+}
+
+export default App;
